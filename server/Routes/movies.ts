61c958e3a2b88b1abf72f4cca072f2b0fa481ae6
Ruben Olano <rubendavid2711@gmail.com>
2022-01-27 08:11:16 -0600

    Add endpoint to add films and endpoint to retreive a random film

--- server/Routes/movies.ts
+++ server/Routes/movies.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { fetchMovie } from '../controllers/movies'
+import { fetchMovie, addMovie } from '../controllers/movies'
 
 const router: express.Router = express.Router()
 
 router.get('/', fetchMovie)
+router.get('/add', addMovie)
 
 export default router
--- server/controllers/movies.ts
+++ server/controllers/movies.ts
@@ -4,8 +4,45 @@ import express from 'express'
 
 export const fetchMovie = async (_req: express.Request, res: express.Response) => {
     try {
-        const movies = await MovieModel.findOne()
-        res.json({ movie: movies })
+        const movies = await MovieModel.aggregate([{ $sample: { size: 1 } }])
+        res.json(movies)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const addMovie = async (_req: express.Request, res: express.Response) => {
+    const Pulp_Fiction = new MovieModel({
+        title: "Pulp Fiction",
+        actors: [{
+            name: "John Travolta",
+            image: "https://m.media-amazon.com/images/M/MV5BMTMyMjZlYzgtZWRjMC00OTRmLTllZTktMmM1ODVmNjljMTQyXkEyXkFqcGdeQXVyMTExNzQ3MzAw._V1_UY317_CR21,0,214,317_AL_.jpg"
+
+        }, {
+            name: "Uma Thurman",
+            image: "https://m.media-amazon.com/images/M/MV5BMjMxNzk1MTQyMl5BMl5BanBnXkFtZTgwMDIzMDEyMTE@._V1_UX214_CR0,0,214,317_AL_.jpg"
+        }, {
+            name: "Samuel L. Jackson",
+            image: "https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg"
+        }, {
+            name: "Bruce Willis",
+            image: "https://m.media-amazon.com/images/M/MV5BMjA0MjMzMTE5OF5BMl5BanBnXkFtZTcwMzQ2ODE3Mw@@._V1_UY317_CR27,0,214,317_AL_.jpg"
+        }, {
+            name: "Tim Roth",
+            image: "https://m.media-amazon.com/images/M/MV5BMjA5NTA3MDQyOV5BMl5BanBnXkFtZTcwODM4NDE3Mw@@._V1_UY317_CR16,0,214,317_AL_.jpg"
+        }, {
+            name: "Amanda Plummer",
+            image: "https://m.media-amazon.com/images/M/MV5BMjAzNDczNzUzMV5BMl5BanBnXkFtZTcwOTQwNTQ1NA@@._V1_UY317_CR131,0,214,317_AL_.jpg"
+        }],
+        poster: "https://cantonpalacetheatre.org/wp-content/uploads/2021/06/Pulp-Fiction.jpg"
+    })
+
+    try {
+        Pulp_Fiction.save((err, movie) => {
+            if (err)
+                return console.log(err)
+            res.json({ movie })
+        })
     } catch (error) {
         console.log(error)
     }
